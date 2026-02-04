04e5c548ab732f3bed8d52273fc332b82103f2d1
Harshit <harshithkdigiverse@gmail.com>
2026-02-04 15:29:26 +0530

    additional charge

---
+++ src/Components/Settings/GeneralSetting/AdditionalCharges/AdditionalChargesForm.tsx
@@ -0,0 +1,8 @@
+const AdditionalChargesForm = () => {
+  return (
+    <>
+      <h1>Additional Charges Form</h1>
+    </>
+  );
+};
+export default AdditionalChargesForm;
---
+++ src/Components/Settings/GeneralSetting/AdditionalCharges/index.tsx
@@ -0,0 +1,8 @@
+const AdditionalCharges = () => {
+  return (
+    <>
+      <h1>Additional Charges</h1>
+    </>
+  );
+};
+export default AdditionalCharges;
