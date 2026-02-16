a88012400724f91e7507f2dede68501ada49683b
Harshit <harshithkdigiverse@gmail.com>
2026-02-16 11:08:50 +0530

    Update in layout

--- src/Data/Layout.tsx
+++ src/Data/Layout.tsx
@@ -38,13 +38,13 @@ export const NavItems: NavItem[] = [
     children: [{ name: PAGE_TITLE.POS.NEW, path: ROUTES.POS.NEW }],
   },
   {
-    name: PAGE_TITLE.SETTINGS.BASE,
-    icon: <SettingsIcon />,
-    children: [{ name: PAGE_TITLE.SETTINGS.GENERAL, path: ROUTES.SETTINGS.GENERAL, pro: false }],
-  },
-  {
     name: PAGE_TITLE.PURCHASE.BASE,
     icon: <SettingsIcon />,
     children: [{ name: PAGE_TITLE.PURCHASE.SUPPLIER_BILL.BASE, path: ROUTES.SUPPLIER_BILL.BASE }],
+  },
+  {
+    name: PAGE_TITLE.SETTINGS.BASE,
+    icon: <SettingsIcon />,
+    children: [{ name: PAGE_TITLE.SETTINGS.GENERAL, path: ROUTES.SETTINGS.GENERAL, pro: false }],
   },
 ];
