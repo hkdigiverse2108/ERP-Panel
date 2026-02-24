309a2f8f395ba77b1bf5e951599b1cc61b90b768
Harshit <harshithkdigiverse@gmail.com>
2026-02-24 16:13:20 +0530

    update in close register

--- src/Components/POS/New/PosHeader/CloseRegister.tsx
+++ src/Components/POS/New/PosHeader/CloseRegister.tsx
@@ -18,7 +18,7 @@ const CloseBillRegister = forwardRef<HTMLDivElement, { bill?: PosOrderBase; data
     try {
       const company = JSON.parse(localStorage.getItem(STORAGE_KEYS.COMPANY) || "null");
       if (company && company.name) return company.name;
-    } catch {}
+    } catch { }
     return "Dhruvi Bakery";
   };
 
@@ -44,19 +44,19 @@ const CloseBillRegister = forwardRef<HTMLDivElement, { bill?: PosOrderBase; data
         </div>
         <div style={{ display: "flex" }}>
           <span style={{ width: "120px" }}>Start Date</span>
-          <span>: {data?.startDate || "31/01/2026"}</span>
+          <span>: {data?.startDate || "-"}</span>
         </div>
         <div style={{ display: "flex" }}>
           <span style={{ width: "120px" }}>Start Time</span>
-          <span>: {data?.startTime || "03:23 PM"}</span>
+          <span>: {data?.startTime || "-"}</span>
         </div>
         <div style={{ display: "flex" }}>
           <span style={{ width: "120px" }}>End Date</span>
-          <span>: {data?.endDate || "18/02/2026"}</span>
+          <span>: {data?.endDate || "-"}</span>
         </div>
         <div style={{ display: "flex" }}>
           <span style={{ width: "120px" }}>End Time</span>
-          <span>: {data?.endTime || "09:59 AM"}</span>
+          <span>: {data?.endTime || "-"}</span>
         </div>
       </div>
 
@@ -64,43 +64,43 @@ const CloseBillRegister = forwardRef<HTMLDivElement, { bill?: PosOrderBase; data
 
       {/* Metrics Section */}
       <div style={{ display: "flex", flexDirection: "column", gap: "4px", marginBottom: "15px" }}>
-        <MetricRow label="No of Bills" value={data?.noOfBills ?? "33.0"} />
-        <MetricRow label="No of Items" value={data?.noOfItems ?? "71.9900"} />
-        <MetricRow label="Total Sales" value={data?.totalSales ?? "12822"} />
-        <MetricRow label="Disc" value={data?.discount ?? "228.77"} />
-        <MetricRow label="Tax Amount" value={data?.taxAmount ?? "1498.0"} />
-        <MetricRow label="Sales Return" value={data?.salesReturn ?? "0.0"} />
-        <MetricRow label="Refund" value={data?.refund ?? "0.0"} />
-        <MetricRow label="Bank Transfer" value={data?.bankTransferAmount ?? "5000.0"} />
-        <MetricRow label="Pay Later" value={data?.payLater ?? "3049.0"} />
-        <MetricRow label="Credit/Advance Redeemed" value={data?.creditAdvanceRedeemed ?? "0.0"} />
-        <MetricRow label="Total Purchase Payment" value={data?.purchasePayment ?? "28.0"} />
-        <MetricRow label="Total Expense" value={data?.expense ?? "500.0"} />
+        <MetricRow label="No of Bills" value={data?.noOfBills ?? 0} />
+        <MetricRow label="No of Items" value={data?.noOfItems ?? 0} />
+        <MetricRow label="Total Sales" value={data?.totalSales ?? 0} />
+        <MetricRow label="Disc" value={data?.discount ?? 0} />
+        <MetricRow label="Tax Amount" value={data?.taxAmount ?? 0} />
+        <MetricRow label="Sales Return" value={data?.salesReturn ?? 0} />
+        <MetricRow label="Refund" value={data?.refund ?? 0} />
+        <MetricRow label="Bank Transfer" value={data?.bankTransferAmount ?? 0} />
+        <MetricRow label="Pay Later" value={data?.payLater ?? 0} />
+        <MetricRow label="Credit/Advance Redeemed" value={data?.creditAdvanceRedeemed ?? 0} />
+        <MetricRow label="Total Purchase Payment" value={data?.purchasePayment ?? 0} />
+        <MetricRow label="Total Expense" value={data?.expense ?? 0} />
       </div>
 
       <div style={{ borderTop: "1px dashed black", marginBottom: "15px" }}></div>
 
       {/* Payment Types Section */}
       <div style={{ display: "flex", flexDirection: "column", gap: "4px", marginBottom: "15px" }}>
-        <MetricRow label="Cash" value={data?.cashPayment ?? "4974.0"} />
-        <MetricRow label="Card" value={data?.cardPayment ?? "649.0"} />
-        <MetricRow label="Wallet" value={data?.walletPayment ?? "0.0"} />
-        <MetricRow label="Upi" value={data?.upiPayment ?? "4150.0"} />
-        <MetricRow label="Bank Refund (-)" value={data?.bankRefund ?? "0.0"} />
-        <MetricRow label="Payments Received" value={data?.paymentsReceived ?? "9773"} />
+        <MetricRow label="Cash" value={data?.cashPayment ?? 0} />
+        <MetricRow label="Card" value={data?.cardPayment ?? 0} />
+        <MetricRow label="Wallet" value={data?.walletPayment ?? 0} />
+        <MetricRow label="Upi" value={data?.upiPayment ?? 0} />
+        <MetricRow label="Bank Refund (-)" value={data?.bankRefund ?? 0} />
+        <MetricRow label="Payments Received" value={data?.paymentsReceived ?? 0} />
       </div>
 
       <div style={{ borderTop: "1px dashed black", marginBottom: "15px" }}></div>
 
       <div style={{ display: "flex", flexDirection: "column", gap: "4px", marginBottom: "15px" }}>
-        <MetricRow label="Total Sales" value={data?.totalSales ?? "12822"} />
+        <MetricRow label="Total Sales" value={data?.totalSales ?? 0} />
       </div>
 
       <div style={{ borderTop: "1px dashed black", marginBottom: "4px" }}></div>
       <div style={{ marginBottom: "4px" }}>Denomination</div>
       <div style={{ borderTop: "1px dashed black", marginBottom: "10px" }}></div>
 
-      {/* Denominations Table */}
+      {/* Denominations Table - Already handled dynamic mapping */}
       <table style={{ width: "100%", borderCollapse: "collapse", textAlign: "left", marginBottom: "30px", tableLayout: "fixed" }}>
         <thead>
           <tr>
@@ -116,7 +116,6 @@ const CloseBillRegister = forwardRef<HTMLDivElement, { bill?: PosOrderBase; data
               <div style={{ borderTop: "1px dashed black", marginBottom: "5px" }}></div>
             </td>
           </tr>
-          {/* Default list based on image */}
           {data?.denominations && data.denominations.length > 0 ? (
             data.denominations.map((denom, idx) => (
               <tr key={idx}>
@@ -128,7 +127,7 @@ const CloseBillRegister = forwardRef<HTMLDivElement, { bill?: PosOrderBase; data
             ))
           ) : (
             <>
-              {[1, 2, 5, 10, 20, 50, 100, 200, 500].map((val) => (
+              {[1, 2, 5, 10, 20, 50, 100, 200, 500, 2000].map((val) => (
                 <tr key={val}>
                   <td style={{ padding: "2px 0" }}>Rs. {val}</td>
                   <td style={{ padding: "2px 0" }}>*</td>
@@ -144,18 +143,18 @@ const CloseBillRegister = forwardRef<HTMLDivElement, { bill?: PosOrderBase; data
       {/* Final Summary Section */}
       <div style={{ display: "flex", flexDirection: "column", gap: "6px", marginBottom: "20px" }}>
         <div style={{ borderTop: "1px dashed black", marginBottom: "2px" }}></div>
-        <MetricRow label="Total Denom" value={data?.totalDenominationAmount ?? "0.0"} />
+        <MetricRow label="Total Denom" value={data?.totalDenominationAmount ?? 0} />
         <div style={{ borderTop: "1px dashed black", marginBottom: "2px", marginTop: "2px" }}></div>
 
-        <MetricRow label="Cash Sales" value={data?.cashPayment ?? "4974.0"} />
-        <MetricRow label="Cash Refund (-)" value={data?.cashRefund ?? "0.0"} />
-        <MetricRow label="Expense Amount (-)" value={data?.expense ?? "500.0"} />
-        <MetricRow label="Cash in Hand" value={data?.cashFlow ?? "13010.0"} />
-        <MetricRow label="Profit*" value={data?.profit ?? "2928.8"} />
+        <MetricRow label="Cash Sales" value={data?.cashPayment ?? 0} />
+        <MetricRow label="Cash Refund (-)" value={data?.cashRefund ?? 0} />
+        <MetricRow label="Expense Amount (-)" value={data?.expense ?? 0} />
+        <MetricRow label="Cash in Hand" value={data?.cashFlow ?? 0} />
+        <MetricRow label="Profit*" value={data?.profit ?? 0} />
 
         <div style={{ borderTop: "1px dashed black", marginBottom: "2px", marginTop: "2px" }}></div>
-        <MetricRow label="System Calculated Cash in Drawer" value={data?.totalCashLeftInDrawer ?? "12456.0"} />
-        <MetricRow label="Actual Cash in Drawer" value={data?.physicalDrawerCash ?? "12456"} />
+        <MetricRow label="System Calculated Cash in Drawer" value={data?.totalCashLeftInDrawer ?? 0} />
+        <MetricRow label="Actual Cash in Drawer" value={data?.physicalDrawerCash ?? 0} />
         <div style={{ borderTop: "1px dashed black", marginBottom: "2px", marginTop: "2px" }}></div>
         <MetricRow label="" value="" />
       </div>
