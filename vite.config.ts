cb47a6097a67ba649dd428eca2df154816e558f0
hk-krish <krishhkdigiverse@gmail.com>
2026-03-11 17:44:34 +0530

    feat: update Vite React plugin to standard version and ignore dev-dist directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ mode }) => {
