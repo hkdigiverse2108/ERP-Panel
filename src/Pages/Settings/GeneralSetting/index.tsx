a3c330b4d53ff5d77ed9eaa947ae7e45146cbeed
shakilhkdigiverse-alt <shakilhkdigiverse@gmail.com>
2026-04-21 15:58:35 +0530

    Push

--- src/Pages/Settings/GeneralSetting/index.tsx
+++ src/Pages/Settings/GeneralSetting/index.tsx
@@ -1,4 +1,4 @@
-import { AccountTree, AddCircle, BarChart, CorporateFare, Payment, Person, ReceiptLong, Tag } from "@mui/icons-material";
+import { AccountTree, AddCircle, BarChart, CorporateFare, Payment, Person, ReceiptLong, Settings, Tag } from "@mui/icons-material";
 import { Box, Grid, Tabs } from "@mui/material";
 import Tab from "@mui/material/Tab";
 import { useState, type SyntheticEvent } from "react";
@@ -29,7 +29,7 @@ const GeneralSetting = () => {
     { label: "User Profile", value: 0, icon: <Person /> },
     { label: "Company Profile", value: 1, icon: <CorporateFare /> },
     ...(TaxPermission.view ? [{ label: PAGE_TITLE.SETTINGS.TAX.TITLE, value: 2, icon: <ReceiptLong /> }] : []),
-    // { label: "Report Formats", value: 3, icon: <Settings /> },
+    { label: "Report Formats", value: 3, icon: <Settings /> },
     ...(RolesPermission.view ? [{ label: PAGE_TITLE.ROLES.TITLE, value: 4, icon: <AccountTree /> }] : []),
     ...(PrefixPermission.view ? [{ label: PAGE_TITLE.SETTINGS.PREFIX.BASE, value: 5, icon: <Tag /> }] : []),
     ...(PaymentTermsPermission.view ? [{ label: PAGE_TITLE.SETTINGS.PAYMENT_TERMS.BASE, value: 6, icon: <Payment /> }] : []),
